d9e705cce2ef731a800439afd2cf63064a1f3974
bakhridinova <bakhridinovaleyla@gmmail.com>
2023-01-27 02:02:09 +0500

    updated StringUtils.java, Utils.java, pom.xml

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,13 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try {
+            if (Double.parseDouble(str) <= 0) {
+                return false;
+            }
+        } catch (NumberFormatException | NullPointerException e) {
+            return false;
+        }
+        return true;
     }
 }
